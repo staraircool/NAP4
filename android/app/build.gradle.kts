336a5650cb27c2a19d636e9f3edcaf68ef33989d
staraircool <295starboy@gmail.com>
2025-08-01 14:18:43 +0500

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.nap4"
+    namespace = "com.napnetworks.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
